76aca1e4e9ecff784fa1c1a2f090e2742e6807e8
junseuk86@gmail.com <junseuk86@gmail.com>
2021-05-03 00:15:57 +0900

    wrote comments

--- buddy-demo.c
+++ buddy-demo.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+// this defines the size of the memory
 #define MAX 16
 
 typedef struct memoryList {
@@ -13,13 +15,30 @@ typedef struct memoryList {
     struct memoryList* prev;
 } memoryList;
 
+/* 
+functions for printing the whole memory and the linked list.
+printBlock() is going to check whether it should be printed or not: 
+a chunck which is split into small chuncks should not be printed.
+*/
 void printMemory(memoryList *this);
 void printBlock(memoryList *this);
 void printList(memoryList *this);
+
+/*
+functions for allocation.
+It loops through the linked list and find a right place to split if needed.
+If not found, it returns an error message.
+*/
 void buddyAlloc(unsigned int numBlock, memoryList *this);
 unsigned int getSizeRequest(unsigned int numBlock);
 int findSplitSpot(memoryList *this, unsigned int sizeRequested);
 void execSplit(memoryList *this);
+
+/*
+functions for freeing blocks.
+It has a similar logic as one used in allocation. But to free blocks, it is necessary to find the right pair.
+And check whether its pair is also empty. If so, a recursive call of checkPair() occurs.
+*/
 int allocFree(memoryList* this, unsigned int sizeRequested, unsigned int numBlock);
 void buddyFree(unsigned int startPosition, memoryList *root);
 void checkPair(memoryList *this);
@@ -27,9 +46,7 @@ memoryList* freeMemory(memoryList* this, memoryList* pair);
 memoryList* findPair(memoryList *this);
 
 int main() {
-    memoryList root = {
-        MAX, 0, 0, 0, 0, NULL, NULL
-    };
+    memoryList root = {MAX, 0, 0, 0, 0, NULL, NULL};
     while(1) {
         char buffer[10];
         char command = 0;
@@ -75,7 +92,10 @@ int main() {
     return 0;
 }
 
-//Allocate Memory
+/*
+Allocate Memory:
+buddyAlloc() takes arguments of numBlock and the pointer to root. It passes the address of root to findSplit().
+*/
 void buddyAlloc(unsigned int numBlock, memoryList *this) {
     memoryList *root = this; //for findSplit();
     unsigned int sizeRequested = getSizeRequest(numBlock);
@@ -88,15 +108,15 @@ void buddyAlloc(unsigned int numBlock, memoryList *this) {
         return;
     }
     while(1){
-        //printf("sizeRequested = %d\n",sizeRequested);
-        //printf("Checking %p\n", this);
         int errCheckallocFree = allocFree(root, sizeRequested, numBlock);
+        //if succeeded to allocate, errCheckallocFree = 0
         if (errCheckallocFree == 0){
             return;
         }
+        //if failed, errCheckallocFree = 1. It means no available space in the memory.
         else if (errCheckallocFree == 1) {
-            //printf("findSplitSpot called\n");
             int errCheckfindSplitSpot = findSplitSpot(root, sizeRequested);
+            //if failed to find a split spot 
             if (errCheckfindSplitSpot == 1) {
                 printf("No space available in memory\n");
                 return;
@@ -110,7 +130,6 @@ int allocFree(memoryList* this, unsigned int sizeRequested, unsigned int numBloc
         if (this -> usedCheck == 0 && this -> size == sizeRequested && this -> splitCheck == 0) {
             this -> usedCheck = 1;
             this -> numBlock = numBlock;
-            //printf("sucessfully allocated %d blocks!\n", this -> numBlock);
             return 0;
         }
         else if (this -> next == NULL) {
@@ -124,8 +143,8 @@ int findSplitSpot(memoryList *root, unsigned int sizeRequested) {
     for(int i = sizeRequested;i <= MAX;i*=2) {
         memoryList* this = root;
         while(1) {
-            unsigned int upperSize = i * 2;
-            if (upperSize == MAX * 2) return 1;
+            unsigned int upperSize = i * 2; //size of split spot
+            if (upperSize == MAX * 2) return 1; //No split spot in the memory
             if (this -> size == upperSize && this -> splitCheck == 0 && this -> usedCheck == 0) {
                 execSplit(this);
                 this -> splitCheck = 1;
@@ -135,34 +154,35 @@ int findSplitSpot(memoryList *root, unsigned int sizeRequested) {
             else break;
         }
     }
-    return 0;
+    return 1;
 }
 
 void execSplit(memoryList *this) {
-    unsigned int newSize = (this -> size)/2;
+    unsigned int splitSize = (this -> size)/2;
     memoryList* nextOfsplit2 = this -> next;
     memoryList* split1;
     memoryList* split2;
     split1 = (memoryList*) malloc(sizeof(memoryList));
     split2 = (memoryList*) malloc(sizeof(memoryList));
-    split1 -> size = newSize;
+    split1 -> size = splitSize;
     split1 -> startPosition = this -> startPosition;
     split1 -> usedCheck = 0;
     split1 -> splitCheck = 0;
     split1 -> numBlock = 0;
     split1 -> next = split2;
     split1 -> prev = this;
-    split2 -> size = newSize;
-    split2 -> startPosition = (this -> startPosition) +  newSize;
+    split2 -> size = splitSize;
+    split2 -> startPosition = (this -> startPosition) +  splitSize;
     split2 -> usedCheck = 0;
     split2 -> splitCheck = 0;
     split2 -> numBlock = 0;
     split2 -> next = nextOfsplit2;
     split2 -> prev = split1;
     this -> next = split1;
-    if (nextOfsplit2 != NULL) nextOfsplit2 -> prev = split2;
+    if (nextOfsplit2 != NULL) nextOfsplit2 -> prev = split2; //when split occurs in the middle of others.
 }
 
+//Find the nearest and smallest power of 2
 unsigned int getSizeRequest(unsigned int numBlock) {
     unsigned int compare = 1;
     while (compare < numBlock) {
@@ -171,7 +191,11 @@ unsigned int getSizeRequest(unsigned int numBlock) {
     return compare;
 }
 
-//Free Memory
+/*
+Free Memory:
+buddyFree() takes startPosition and the address of root for the loop.
+Before freeing blocks, it has to find the right pair and if the pair, another one, exists after or before.
+*/
 void buddyFree (unsigned int startPosition, memoryList *root) {
     memoryList* this = root;
     while(1) {
@@ -196,22 +220,26 @@ void buddyFree (unsigned int startPosition, memoryList *root) {
 
 void checkPair (memoryList *this) {
     memoryList *pair = findPair(this);
+    //if no pair found
     if (pair == NULL) {
         this -> usedCheck = 0;
         this -> numBlock = 0;
         return;
     }
+    //if another one is used or split
     else if (pair -> usedCheck == 1 || pair -> splitCheck == 1) {
         this -> usedCheck = 0;
         this -> numBlock = 0;
         return;
     }
+    //if the pair is both free, merge. Recursion to check if further merging is needed.
     else if (pair -> usedCheck == 0) {
         memoryList* merged = freeMemory(this, pair);
         checkPair(merged);
     }
 }
 
+//Find a pair using size
 memoryList* findPair(memoryList *this) {
     memoryList *prev = this -> prev;
     memoryList *next = this -> next;
@@ -248,13 +276,16 @@ memoryList* freeMemory (memoryList* this, memoryList* pair) {
 void printMemory(memoryList *this) {
     printf("|");
     while(1) {
+        //End of the linked list
         if (this -> next == NULL) {
             printBlock(this);
             return;
         }
+        //Used ones
         else if (this -> usedCheck == 1 && this -> splitCheck == 0) {
             printBlock(this);
         }
+        //Empty ones
         else if (this -> usedCheck == 0 && this -> splitCheck == 0) {
             printBlock(this);
         }
@@ -275,6 +306,7 @@ void printBlock(memoryList *this) {
     printf("|");
 }
 
+//Print the whole linked list with description.
 void printList(memoryList *this) {
     printf("\n");
     printf("In order of\n");
